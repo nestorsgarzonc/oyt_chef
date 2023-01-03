5de45ba96b5fdfed1351b831cad005290382511f
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-03 11:45:07 -0500

    Added submodules

---
+++ config_packages.sh
@@ -0,0 +1,11 @@
+cd packages
+git submodule add https://github.com/nestorsgarzonc/oyt_front_auth
+git submodule add https://github.com/nestorsgarzonc/oyt_front_core
+git submodule add https://github.com/nestorsgarzonc/oyt_front_menu
+git submodule add https://github.com/nestorsgarzonc/oyt_front_order
+git submodule add https://github.com/nestorsgarzonc/oyt_front_product
+git submodule add https://github.com/nestorsgarzonc/oyt_front_restaurant
+git submodule add https://github.com/nestorsgarzonc/oyt_front_table
+git submodule add https://github.com/nestorsgarzonc/oyt_front_widgets
+git submodule add https://github.com/nestorsgarzonc/oyt_front_orders_queue
+flutter pub get
